d488d6346e312d44c69277dae1eabb87928b1f36
Corenzyk K. <corentin.kuczyk49@gmail.com>
2024-09-19 14:29:44 +0200

    Ajout des méthodes de login et de register, avec leur méthode de traitement respective

--- app/Http/Controllers/AuthentificationControleur.php
+++ app/Http/Controllers/AuthentificationControleur.php
@@ -2,27 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Utilisateur;
 use Illuminate\Http\Request;
 
 class AuthentificationControleur extends Controller
 {
     public function login(Request $request)
     {
-
+        return view('login', []);
     }
 
     public function traitementLogin(Request $request)
     {
+        $mdp = $request->input('password');
+        $email = $request->input('email');
+        $utilisateur = Utilisateur::where('email', $email)->first();
+        $estValide = password_verify($mdp, $utilisateur->password);
 
+        if ($estValide) {
+            $request->session()->put('user', $utilisateur);
+        }
+        else {
+            return redirect('/login')->with('error', 'Identifiants incorrects');
+        }
     }
 
     public function register(Request $request)
     {
-
+        return view('register', []);
     }
 
     public function traitementRegister(Request $request)
     {
-
+        $mdp = $request->input('password');
+        $hash = password_hash($mdp, PASSWORD_DEFAULT);
     }
 }
