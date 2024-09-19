3c3e59293e7c76a4d9519925683441f524bcbb14
Corenzyk K. <corentin.kuczyk49@gmail.com>
2024-09-19 14:36:47 +0200

    Création du middleware CheckAuth et application du middleware au routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PingPongControleur;
 use App\Http\Controllers\TestFlashController;
 use App\Http\Controllers\TodoControleur;
+use App\Http\Middleware\CheckAuth;
 use App\Http\Middleware\CheckTodo;
 use Illuminate\Support\Facades\Route;
 
@@ -19,13 +20,13 @@ Route::get('/flash', [TestFlashController::class, 'main']);
 Route::post('/traitement', [TestFlashController::class, 'traitement']);
 
 Route::get('/layout', [TodoControleur::class, 'listTodo']);
-Route::post('/layout', [TodoControleur::class, 'addTodo'])->middleware(CheckTodo::class);
+Route::post('/layout', [TodoControleur::class, 'addTodo'])->middleware(CheckAuth::class)->middleware(CheckTodo::class);
 
-Route::get('/termine/{id}', [TodoControleur::class, 'markAsDone']);
-Route::get('/supprime/{id}', [TodoControleur::class, 'deleteTodo']);
+Route::get('/termine/{id}', [TodoControleur::class, 'markAsDone'])->middleware(CheckAuth::class);
+Route::get('/supprime/{id}', [TodoControleur::class, 'deleteTodo'])->middleware(CheckAuth::class);
 
 Route::get('/contact',  [ContactController::class, 'afficheForm']);
-Route::post('/contact',  [ContactController::class, 'addMail']);
+Route::post('/contact',  [ContactController::class, 'addMail'])->middleware(CheckAuth::class);
 
 Route::get('/login', [AuthentificationControleur::class, 'login']);
 Route::post('/traitementLogin', [AuthentificationControleur::class, 'traitementLogin']);
